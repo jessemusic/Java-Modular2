61e7b47849c524b36fc4310399205e0d3d4911b1
jessemusic <jmc_cursos@hotmail.com>
2021-02-15 23:02:29 -0300

    Finalizando aula 4

--- digital.innovation.one.jesse.core/src/digital/innovation/one/jesse/core/Runner.java
+++ digital.innovation.one.jesse.core/src/digital/innovation/one/jesse/core/Runner.java
@@ -4,10 +4,12 @@ package digital.innovation.one.jesse.core;
 import digital.innovation.one.jesse.utils.operacao.Calculadora;
 
 
+
 public class Runner {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         System.out.println(calculadora.mult( 3,4 ));
+
 
 
     }
